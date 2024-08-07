bc986db6d209547f10e173fef55a30c7074fc30c
matchima <mkhamyod@gmail.com>
2024-08-07 16:13:45 +0700

    Q3

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import StudentListView from '@/views/StudentListView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 
 const routes = [
   {
@@ -18,22 +19,31 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'event-detail-view',
-    component: EventDetailView,
-    props: true
+    name: 'event-layout-view',
+    component: EventLayoutView,
+    props: true,
+    children: [
+      {
+        path: '',
+        name: 'event-detail-view',
+        component: EventDetailView,
+        props: true
+      },
+      {
+        path: 'register',
+        name: 'event-register-view',
+        component: EventRegisterView,
+        props: true
+      },
+      {
+        path: 'edit',
+        name: 'event-edit-view',
+        component: EventEditView,
+        props: true
+      }
+    ]
   },
-  {
-    path: '/event/:id/register',
-    name: 'event-register-view',
-    component: EventRegisterView,
-    props: true
-  },
-  {
-    path: '/event/:id/edit',
-    name: 'event-edit-view',
-    component: EventEditView,
-    props: true
-  },
+
   {
     path: '/about',
     name: 'about',
